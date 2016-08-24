e9d12fa9830bdd54cd2198bd3588982dcb027a81
alexxero <alexxero@mail.ru>
2016-08-24 10:40:51 +0000

    admin column fix

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160820150219) do
+ActiveRecord::Schema.define(version: 20160824103807) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -66,7 +66,6 @@ ActiveRecord::Schema.define(version: 20160820150219) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name"
-    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
