1d3e7032cdc18cf621c49bc7db23e47895025873
alexxero <alexxero@mail.ru>
2016-09-12 14:47:06 +0300

    Dropped Questions table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160911145851) do
+ActiveRecord::Schema.define(version: 20160912110320) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -44,13 +44,6 @@ ActiveRecord::Schema.define(version: 20160911145851) do
   end
 
   add_index "posts", ["slug"], name: "index_posts_on_slug", unique: true
-
-  create_table "questions", force: :cascade do |t|
-    t.string   "title"
-    t.text     "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
